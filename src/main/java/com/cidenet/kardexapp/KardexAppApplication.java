5cdca96656f8ff643f729d2c775c93c695f53506
EDISSONG <eagarcia@soaint.com>
2020-04-17 15:25:51 -0500

    Initial commit

---
+++ src/main/java/com/cidenet/kardexapp/KardexAppApplication.java
@@ -0,0 +1,13 @@
+package com.cidenet.kardexapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KardexAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KardexAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cidenet/kardexapp/KardexAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cidenet.kardexapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KardexAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
